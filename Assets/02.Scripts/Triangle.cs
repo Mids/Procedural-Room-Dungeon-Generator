fc04d858134b6b904049536b214f95778bf220aa
Jin Park <jiny199109@gmail.com>
2016-07-11 00:40:46 +0900

    Improve circumcircle calculating speed

--- Assets/02.Scripts/Triangle.cs
+++ Assets/02.Scripts/Triangle.cs
@@ -7,6 +7,9 @@ public class Triangle
 	public List<Room> Rooms = new List<Room>();
 	public List<Corridor> Corridors = new List<Corridor>();
 
+	private Vector3 _circumcenter = Vector3.zero;
+	private float _radius;
+
 	public Triangle(Room r1, Room r2, Room r3)
 	{
 		Rooms.Add(r1);
@@ -28,38 +31,41 @@ public class Triangle
 	/// <returns> True if the point is in this triangle's circumcircle </returns>
 	public bool IsContaining(Room room)
 	{
-		Vector3[] vertexs = new Vector3[3];
-		for (int index = 0; index < Rooms.Count; index++)
+		// Save calculated circumcenter.
+		if (_circumcenter == Vector3.zero)
 		{
-			vertexs[index] = Rooms[index].transform.localPosition;
+			Vector3[] vertexs = new Vector3[3];
+			for (int index = 0; index < Rooms.Count; index++)
+			{
+				vertexs[index] = Rooms[index].transform.localPosition;
+			}
+
+			float a = vertexs[1].x - vertexs[0].x;
+			float b = vertexs[1].z - vertexs[0].z;
+			float c = vertexs[2].x - vertexs[0].x;
+			float d = vertexs[2].z - vertexs[0].z;
+
+			float aux1 = a * (vertexs[0].x + vertexs[1].x) + b * (vertexs[0].z + vertexs[1].z);
+			float aux2 = c * (vertexs[0].x + vertexs[2].x) + d * (vertexs[0].z + vertexs[2].z);
+			float div = 2.0f * (a * (vertexs[2].z - vertexs[1].z) - b * (vertexs[2].x - vertexs[1].x));
+
+			if (Math.Abs(div) < float.Epsilon)
+			{
+				Debug.LogError("Divided by Zero : " + div);
+				return false;
+			}
+
+			_circumcenter = new Vector3((d * aux1 - b * aux2) / div, 0, (a * aux2 - c * aux1) / div);
+			_radius = Mathf.Sqrt((_circumcenter.x - vertexs[0].x) * (_circumcenter.x - vertexs[0].x) + (_circumcenter.z - vertexs[0].z) * (_circumcenter.z - vertexs[0].z));
 		}
 
-		float a = vertexs[1].x - vertexs[0].x;
-		float b = vertexs[1].z - vertexs[0].z;
-		float c = vertexs[2].x - vertexs[0].x;
-		float d = vertexs[2].z - vertexs[0].z;
-
-		float aux1 = a * (vertexs[0].x + vertexs[1].x) + b * (vertexs[0].z + vertexs[1].z);
-		float aux2 = c * (vertexs[0].x + vertexs[2].x) + d * (vertexs[0].z + vertexs[2].z);
-		float div = 2.0f * (a * (vertexs[2].z - vertexs[1].z) - b * (vertexs[2].x - vertexs[1].x));
-
-		if (Math.Abs(div) < float.Epsilon)
-		{
-			Debug.LogError("Divided by Zero : " + div);
-			return false;
-		}
-
-		Vector3 center = new Vector3((d * aux1 - b * aux2) / div, 0, (a * aux2 - c * aux1) / div);
-		float radius = Mathf.Sqrt((center.x - vertexs[0].x) * (center.x - vertexs[0].x) + (center.z - vertexs[0].z) * (center.z - vertexs[0].z));
-
-		if (Vector3.Distance(room.transform.localPosition, center) > radius)
+		if (Vector3.Distance(room.transform.localPosition, _circumcenter) > _radius)
 		{
 			return false;
 		}
 
 		return true;
 	}
-
 
 	public void Show()
 	{
